8bd533eca8199c1d1edfee3e299c866c854bc7b0
r-pluss <r.pluss52@gmail.com>
2016-11-09 15:16:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+'use strict';
 
+const app = require('app');
+const BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800
+    });
+    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+});
